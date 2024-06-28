f918f3df50747e7d82b08f7841e6c1f6a7519731
Priscila Sayuri Egashira Nitta-PNITTA <pnitta@motorola.com>
2024-06-27 21:14:40 -0300

    predict and errors

--- aulaRegressaoLinearSimples/regressao_simples.py
+++ aulaRegressaoLinearSimples/regressao_simples.py
@@ -16,3 +16,23 @@ plt.show()
 
 dados.corr()
 
+X = dados[['Temperatura']]
+y = dados['Vendas_Sorvetes']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+previsoes = modelo.predict(X_test)
+
+erro_medio_quadratico = mean_squared_error(y_test, previsoes)
+erro_absoluto_medio = mean_absolute_error(y_test, previsoes)
+r_quadrado = r2_score(y_test, previsoes)
+
+print(f'erro médio quadrático: {erro_medio_quadratico}')
+print(f'erro absoluto médio: {erro_absoluto_medio}')
+print(f'R² (coeficiente de determinação): {r_quadrado}')
+
+plt.scatter(X_test, y_test, label = 'real')
+plt.scatter(X_test, previsoes, label='Previsto', color='red')
+plt.xlabel('Temperatura ºC')
